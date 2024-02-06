b9c66050c461ee56d2e250fbcedb4b3c8a2d537e
Franco Tosoni Marquez <francotosoni@hotmail.com>
2024-02-06 17:56:42 -0300

    starting

---
+++ app/(general)/contact/page.tsx
@@ -0,0 +1,14 @@
+
+export const metadata = {
+    title: 'SEO Title',
+    description: 'SEO Description',
+    keywords: ['Contact Page', 'Franco'],
+   };
+
+   export default function ContactPage() {
+    return (
+        <>
+            <span className="text-7xl">Contact Page</span>
+        </>
+    )
+}
